376cf81e1178c7c22268cf9b4a6f375ea94c6eea
theodoreKor <thkoronaios@gmail.com>
2018-05-17 22:38:13 +0300

    initial commit

---
+++ SignalR_chat_example/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR_chat_example
+{
+    public class ChatHub : Hub
+    {
+        public void Broadcast(string message)
+        {
+            Clients.All.receive(Context.ConnectionId, message);
+        }
+
+        public void DeleteAll()
+        {
+            Clients.All.delete();
+        }
+    }
+}
